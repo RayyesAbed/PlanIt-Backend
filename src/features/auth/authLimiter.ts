6d24e96fb84ae8e930b8232507b363f83b7f9a5f
RayyesAbed <abedray1567@gmail.com>
2025-08-30 18:32:13 +0200

    Added a Login rate limiter

--- src/features/auth/authLimiter.ts
+++ src/features/auth/authLimiter.ts
@@ -5,3 +5,9 @@ export const registerRateLimiter = rateLimit({
   limit: 5, // Only 5 possible register attempts per 1 hour
   message: "Too many register attempts! Please try again later",
 });
+
+export const loginRateLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // Per 1 hour
+  limit: 3, // Only 3 possible login attempts per 1 hour
+  message: "Too many login attempts! Please try again later",
+});
