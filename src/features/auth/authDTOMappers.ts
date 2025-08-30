1d46fe9159a90d87f8518cae13e00b77448cf320
RayyesAbed <abedray1567@gmail.com>
2025-08-30 18:33:22 +0200

    Added a 'toLoginDTO' function for returning LoginDTO type

--- src/features/auth/authDTOMappers.ts
+++ src/features/auth/authDTOMappers.ts
@@ -1,9 +1,16 @@
-import { RegisterRequestDTO } from "./authDTOs";
+import { LoginDTO, RegisterRequestDTO } from "./authDTOs";
 
 export const toRegisterDTO = (credentials: any): RegisterRequestDTO => {
   return {
     name: credentials.name,
     toBeConfirmedEmail: credentials.toBeConfirmedEmail,
+    password: credentials.password,
+  };
+};
+
+export const toLoginDTO = (credentials: any): LoginDTO => {
+  return {
+    email: credentials.email,
     password: credentials.password,
   };
 };
