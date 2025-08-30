4eab62e401106dfd1f3e39cc04bb02d70c0c7037
RayyesAbed <abedray1567@gmail.com>
2025-08-30 18:35:27 +0200

    Used CORS to allow some origins to access the backend in index file, and passed auth routes in /auth endpoint

--- index.ts
+++ index.ts
@@ -4,12 +4,23 @@ dotenv.config({ path: ".env.development" });
 
 // Then import modules and functions
 import express from "express";
+import cors from "cors";
 import mongooseConnect from "./src/configs/mongooseConnect";
+import authRoutes from "./src/features/auth/authRoutes";
 
 const app = express();
 
 const launchBackendServer = async () => {
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+      credentials: true,
+    })
+  );
+
   await mongooseConnect();
+  app.use("/auth", authRoutes);
   app.listen(3000);
 };
 
