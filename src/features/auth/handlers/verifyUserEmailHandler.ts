cde6197aeefbe78f79d447443454c0383323357e
RayyesAbed <abedray1567@gmail.com>
2025-08-30 18:23:06 +0200

    Created user email verification handler:
    - Extracted token from query
    - Verified JWT and got user ID
    - Updated 'confirmedEmail' with 'toBeConfirmedEmail'
    - Cleared 'toBeConfirmedEmail'
    - Handled errors with proper 500 response

---
+++ src/features/auth/handlers/verifyUserEmailHandler.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import User from "../../../schemas/User";
+import { getEnvVariables } from "../../../configs/enviromentVariables";
+
+export const verifyUserEmailHandler = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  // Load the JWT Secret
+  const { JWT_SECRET } = getEnvVariables();
+
+  // Get the token from the request and cast it to string
+  const token = req.query.token as string;
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+
+    if (typeof payload === "string") {
+      return res.status(400).json({ message: "Invalid token format" });
+    }
+
+    // If JWT token format is valid, then get the user Id
+    const userId = payload.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update the user confirmed email and to be confirmed email
+    await user?.updateOne({
+      confirmedEmail: user.toBeConfirmedEmail,
+      toBeConfirmedEmail: "",
+    });
+
+    // TODO: Create a User Tasks document after successfully creating the user document
+
+    return res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    console.error("Error verifying JWT token:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
