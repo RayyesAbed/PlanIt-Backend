cb48f324c70593ddf7d9db5cec36b0193fc271b9
RayyesAbed <abedray1567@gmail.com>
2025-09-29 15:43:01 +0200

    Added AppSecrets type for secrets fetched from AWS Secrets Manager

---
+++ src/types/AppSecrets.ts
@@ -0,0 +1,13 @@
+type AppSecrets = {
+  MONGODB_URI: string;
+  GEMINI_API_KEY: string;
+  AWS_S3_BUCKET: string;
+  AWS_REGION: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_ACCESS_KEY_ID: string;
+  GMAIL_USER: string;
+  GMAIL_PASSWORD: string;
+  JWT_SECRET: string;
+};
+
+export default AppSecrets;
