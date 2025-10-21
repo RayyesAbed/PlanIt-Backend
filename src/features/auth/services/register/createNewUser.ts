9d369b6918479ea8fc0cf6835f243e14ed1b2666
RayyesAbed <abedray1567@gmail.com>
2025-10-21 11:11:13 +0200

    Outsourced createNewUser function for better code maintainability

---
+++ src/features/auth/services/register/createNewUser.ts
@@ -0,0 +1,31 @@
+import * as argon2 from "argon2";
+import { RegisterRequestDTO } from "../../authDTOs";
+import Subscription from "../../../../schemas/Subscription";
+import { getCurrencyFromIP } from "../../../../utils/currency";
+import User from "../../../../schemas/User";
+
+const createNewUser = async (
+  registerCredentialsDTO: RegisterRequestDTO,
+  deviceIPv4: string
+) => {
+  const hashedPassword = await argon2.hash(registerCredentialsDTO.password);
+  const freePlan = await Subscription.findOne({ name: "Free" });
+
+  if (!freePlan) throw new Error("Free subscription plan not found");
+
+  const currencySymbol = getCurrencyFromIP(deviceIPv4);
+
+  const newUser = await User.create({
+    name: registerCredentialsDTO.name,
+    toBeConfirmedEmail: registerCredentialsDTO.toBeConfirmedEmail,
+    birthDate: registerCredentialsDTO.birthDate,
+    preferredLanguage: registerCredentialsDTO.preferredLanguage,
+    subscription: freePlan._id,
+    currency: currencySymbol,
+    password: hashedPassword,
+  });
+
+  return newUser;
+};
+
+export default createNewUser;
