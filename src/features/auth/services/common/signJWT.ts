fe8ffdb02aadbef9019880c94f2d1505c146ac4f
RayyesAbed <abedray1567@gmail.com>
2025-10-21 11:12:34 +0200

    Outsourced signJWT function for better code maintainability

---
+++ src/features/auth/services/common/signJWT.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { RegisterRequestDTO } from "../../authDTOs";
+import loadAwsSecrets from "../../../../configs/loadAwsSecrets";
+import { randomUUID } from "crypto";
+import { Types } from "mongoose";
+
+const signJWT = async (
+  registerCredentialsDTO: RegisterRequestDTO,
+  jwtSignPurpose: string,
+  newUserId: Types.ObjectId
+) => {
+  const { JWT_SECRET } = await loadAwsSecrets();
+  const verificationToken = jwt.sign(
+    {
+      userId: newUserId,
+      lang: registerCredentialsDTO.preferredLanguage,
+      purpose: jwtSignPurpose,
+    },
+    JWT_SECRET,
+    {
+      expiresIn: "1h",
+      jwtid: randomUUID(),
+    }
+  );
+
+  return verificationToken;
+};
+
+export default signJWT;
