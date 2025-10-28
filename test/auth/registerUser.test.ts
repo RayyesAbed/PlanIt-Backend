8ea73fd0fce3b4297c9fd4956c6e7e9edad3ef52
RayyesAbed <abdullarayes00@gmail.com>
2025-10-29 00:25:49 +0100

    Created registerUser test file and imported files and functions to be used for testing user registration

---
+++ test/auth/registerUser.test.ts
@@ -0,0 +1,3 @@
+import { beforeEach, vi, describe, it, afterEach } from "vitest";
+import { registerUserRequestHandler } from "../../src/features/auth/handlers/registerUserHandler";
+import User from "../../src/schemas/User";
