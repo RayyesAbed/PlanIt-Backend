c61b42d3c7010c735511b818170e99a33aefdaf1
RayyesAbed <abdullarayes00@gmail.com>
2025-11-28 17:46:35 +0100

    Adjusted mockRequest function to include (body, params, query, headers, and ip)

--- test/utils/mockExpress.ts
+++ test/utils/mockExpress.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { vi } from "vitest";
 
-export function mockRequest(body: any): Partial<Request> {
-  return { body };
-}
+export const mockRequest = (body = {}) => ({
+  body,
+  params: {},
+  query: {},
+  headers: {},
+  ip: "::1"
+});
 
 export function mockResponse(): Response {
   const res = {} as Response;
