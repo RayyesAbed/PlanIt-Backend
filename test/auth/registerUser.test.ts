96ecd189d07f9e2629506b2afe80f612360805c8
RayyesAbed <abdullarayes00@gmail.com>
2025-11-28 15:08:03 +0100

    Added a mock for User schema in registerUser test file

--- test/auth/registerUser.test.ts
+++ test/auth/registerUser.test.ts
@@ -1,3 +1,5 @@
 import { beforeEach, vi, describe, it, afterEach } from "vitest";
 import { registerUserRequestHandler } from "../../src/features/auth/handlers/registerUserHandler";
 import User from "../../src/schemas/User";
+
+vi.mock("../../src/schemas/User");
