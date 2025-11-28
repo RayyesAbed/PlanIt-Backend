e31c2ac6eb296c04582f5109defafd1687301abf
RayyesAbed <abdullarayes00@gmail.com>
2025-11-28 15:06:05 +0100

    Added function for mocking responses in mockExpress file

--- test/utils/mockExpress.ts
+++ test/utils/mockExpress.ts
@@ -5,3 +5,11 @@ export function mockRequest(body: any): Partial<Request> {
   return { body };
 }
 
+export function mockResponse(): Response {
+  const res = {} as Response;
+
+  res.status = vi.fn().mockReturnValue(res);
+  res.json = vi.fn().mockReturnValue(res);
+
+  return res;
+}
