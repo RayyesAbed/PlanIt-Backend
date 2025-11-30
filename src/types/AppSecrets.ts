b4e399c561161ab00430e2a1180dc77f3c2ce47a
RayyesAbed <abdullarayes00@gmail.com>
2025-11-30 18:04:52 +0100

    Removed AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY properties from AppSecrets type

--- src/types/AppSecrets.ts
+++ src/types/AppSecrets.ts
@@ -3,8 +3,6 @@ type AppSecrets = {
   GEMINI_API_KEY: string;
   AWS_S3_BUCKET: string;
   AWS_REGION: string;
-  AWS_SECRET_ACCESS_KEY: string;
-  AWS_ACCESS_KEY_ID: string;
   GMAIL_USER: string;
   GMAIL_PASSWORD: string;
   JWT_SECRET: string;
