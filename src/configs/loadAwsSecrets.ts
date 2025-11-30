04f396f9561cc05416ebd7f0868acb1a1e7eff87
RayyesAbed <abdullarayes00@gmail.com>
2025-11-30 17:56:37 +0100

    Removed AWS Secrets Manager import and setup and changed function name to loadSecrets to load them from .env file

--- src/configs/loadAwsSecrets.ts
+++ src/configs/loadAwsSecrets.ts
@@ -1,29 +1,9 @@
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
 import AppSecrets from "../types/AppSecrets";
 
-const client = new SecretsManagerClient({
-  region: process.env.AWS_SECRET_KEYS_REGION,
-});
-
 /**
- * Loads and parses secrets from AWS Secrets Manager.
- * @returns {Promise<AppSecrets>} Parsed secret values.
+ * Loads and parses secrets from .env file.
+ * @returns {AppSecrets} Parsed secret values.
  */
-const loadAwsSecrets = async (): Promise<AppSecrets> => {
-  const command = new GetSecretValueCommand({
-    SecretId: process.env.ARN,
-  });
+const loadSecrets = (): AppSecrets => {};
 
-  const response = await client.send(command);
-
-  if (!response.SecretString) throw new Error("Loading secrets failed!");
-
-  const secrets: AppSecrets = JSON.parse(response.SecretString);
-
-  return secrets;
-};
-
-export default loadAwsSecrets;
+export default loadSecrets;
