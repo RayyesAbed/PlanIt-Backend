70378f147d8505dad17782945d308378edb5bdbb
RayyesAbed <abdullarayes00@gmail.com>
2025-11-30 18:08:58 +0100

    Returned secrets used in PlanIt backend with loadSecrets function

--- src/configs/loadSecrets.ts
+++ src/configs/loadSecrets.ts
@@ -10,6 +10,16 @@ function assertEnv(varName: string): string {
  * Loads and parses secrets from .env file.
  * @returns {AppSecrets} Parsed secret values.
  */
-const loadSecrets = (): AppSecrets => {};
+const loadSecrets = (): AppSecrets => {
+  return {
+    MONGODB_URI: assertEnv("MONGODB_URI"),
+    GEMINI_API_KEY: assertEnv("GEMINI_API_KEY"),
+    AWS_S3_BUCKET: assertEnv("AWS_S3_BUCKET"),
+    AWS_REGION: assertEnv("AWS_REGION"),
+    GMAIL_USER: assertEnv("GMAIL_USER"),
+    GMAIL_PASSWORD: assertEnv("GMAIL_PASSWORD"),
+    JWT_SECRET: assertEnv("JWT_SECRET"),
+  };
+};
 
 export default loadSecrets;
