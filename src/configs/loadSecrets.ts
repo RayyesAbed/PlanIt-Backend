75e377a7d98f6175b7ec9ce0e09bb2935b3ff39c
RayyesAbed <abdullarayes00@gmail.com>
2025-11-30 18:02:40 +0100

    Added asserEnv function to return existing .env variables or throw an error about missing variable name

--- src/configs/loadSecrets.ts
+++ src/configs/loadSecrets.ts
@@ -1,5 +1,11 @@
 import AppSecrets from "../types/AppSecrets";
 
+function assertEnv(varName: string): string {
+  const value = process.env[varName];
+  if (!value) throw new Error(`Missing environment variable: ${varName}`);
+  return value;
+}
+
 /**
  * Loads and parses secrets from .env file.
  * @returns {AppSecrets} Parsed secret values.
