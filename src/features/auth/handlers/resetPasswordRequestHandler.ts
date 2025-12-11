eef52782bde6bf19dd862412d7202eac30208d33
RayyesAbed <abdullarayes00@gmail.com>
2025-12-11 12:56:10 +0100

    Changed 'email' to 'confirmedEmail' for clarity, and searched user through 'confirmedEmail' filter

--- src/features/auth/handlers/resetPasswordRequestHandler.ts
+++ src/features/auth/handlers/resetPasswordRequestHandler.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
+import User from "../../../schemas/User";
 
-export const resetPasswordRequestHandler = (req: Request, res: Response) => {
-  const email: string = req.body.email;
+export const resetPasswordRequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const confirmedEmail: string = req.body.email;
+
+  const user = await User.findOne({ confirmedEmail: confirmedEmail });
 };
