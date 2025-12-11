095c405f03cf362bcdfc95dfcf09b1829aa085e1
RayyesAbed <abdullarayes00@gmail.com>
2025-12-11 13:06:35 +0100

    Added 'if' conditional (to add its logic next) and returned 200 status with code 'SUCCESS'

--- src/features/auth/handlers/resetPasswordRequestHandler.ts
+++ src/features/auth/handlers/resetPasswordRequestHandler.ts
@@ -4,8 +4,13 @@ import User from "../../../schemas/User";
 export const resetPasswordRequestHandler = async (
   req: Request,
   res: Response
-) => {
+): Promise<any> => {
   const confirmedEmail: string = req.body.email;
 
   const user = await User.findOne({ confirmedEmail: confirmedEmail });
+
+  if (user) {
+  }
+
+  return res.status(200).json({ code: "SUCCESS" });
 };
