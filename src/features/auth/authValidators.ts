8324e222fa5c91efd9968206062000af59265020
RayyesAbed <abdullarayes00@gmail.com>
2025-12-13 18:53:30 +0100

    Added resetPasswordValidator in authValidators to validate new password before passing to resetPasswordHandler

--- src/features/auth/authValidators.ts
+++ src/features/auth/authValidators.ts
@@ -45,3 +45,19 @@ export const loginValidator = [
     .withMessage("Email must be a valid email address"),
   body("password").notEmpty().withMessage("Password is required"),
 ];
+
+export const resetPasswordValidator = [
+  body("newPassword")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .matches(/\d/)
+    .withMessage("Password must contain a number")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain a lowercase letter")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain an uppercase letter")
+    .matches(/[^A-Za-z0-9]/)
+    .withMessage("Password must contain a special character")
+    .custom(rejectDangerousCharacters),
+];
