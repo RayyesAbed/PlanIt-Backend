0eed45f28e8dbf0e5860a0ea6b9da9be9b98cf15
RayyesAbed <abdullarayes00@gmail.com>
2025-12-13 19:25:32 +0100

    Used validateInputs in resetPasswordHandler and wrapped it in its try..catch block

--- src/features/auth/handlers/resetPasswordHandler.ts
+++ src/features/auth/handlers/resetPasswordHandler.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
+import validateInputs from "../utils/validateInputs";
 
 export const resetPasswordHandler = async (
   req: Request,
   res: Response
-): Promise<any> => {};
+): Promise<any> => {
+  try {
+    validateInputs(req);
+  } catch (error: any) {
+    return res.status(400).json({ message: error });
+  }
+};
