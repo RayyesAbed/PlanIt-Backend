04023716331a20c476a6c1d2e5df7cd20fffd9db
RayyesAbed <abdullarayes00@gmail.com>
2025-12-14 13:32:08 +0100

    Added TokenAlreadyUsedError and TokenNotFoundError that extend Error class and used them on already used JTI and non-existing JTI

--- src/features/auth/services/common/getAndRevokeRedisKey.ts
+++ src/features/auth/services/common/getAndRevokeRedisKey.ts
@@ -1,18 +1,31 @@
 import { redis } from "../../../../configs/redis";
-import emailVerificationCodes from "../../../../types/emailVerificationCodes";
 import setRedisKey from "./setRedisKey";
 
-const getAndRevokeRedisKey = async (jti: string) => {
+export class TokenAlreadyUsedError extends Error {
+  constructor() {
+    super("TOKEN_ALREADY_USED");
+  }
+}
+
+export class TokenNotFoundError extends Error {
+  constructor() {
+    super("TOKEN_NOT_FOUND");
+  }
+}
+
+const getAndRevokeRedisKey = async (jti: string): Promise<void> => {
   const value = await redis.get(`jti=${jti}`);
 
   if (value === "false") {
     await setRedisKey(jti, "true");
-    return true;
-  } else if (value === "true") {
-    throw new Error(emailVerificationCodes.ALREADY_VERIFIED);
-  } else {
-    throw new Error(emailVerificationCodes.INVALID_TOKEN);
+    return;
   }
+
+  if (value === "true") {
+    throw new TokenAlreadyUsedError();
+  }
+
+  throw new TokenNotFoundError();
 };
 
 export default getAndRevokeRedisKey;
