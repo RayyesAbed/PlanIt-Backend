660ab63f84878ad218eb903a5b9d40b0c9da0ef5
RayyesAbed <abdullarayes00@gmail.com>
2025-12-14 13:38:53 +0100

    Used getAndRevokeRedisKey and handled its 409 401 and 500 errors

--- src/features/auth/handlers/resetPasswordHandler.ts
+++ src/features/auth/handlers/resetPasswordHandler.ts
@@ -3,6 +3,11 @@ import validateInputs from "../utils/validateInputs";
 import verifyJWT from "../services/common/verifyJWT";
 import jwt from "jsonwebtoken";
 import emailVerificationCodes from "../types/emailVerificationCodes";
+import getAndRevokeRedisKey, {
+  TokenAlreadyUsedError,
+  TokenNotFoundError,
+} from "../services/common/getAndRevokeRedisKey";
+import PasswordResetCode from "../types/PasswordResetCode";
 
 export const resetPasswordHandler = async (
   req: Request,
@@ -25,4 +30,26 @@ export const resetPasswordHandler = async (
       .status(401)
       .json({ message: emailVerificationCodes.INVALID_TOKEN });
   }
+
+  try {
+    await getAndRevokeRedisKey(decoded.jti as string);
+  } catch (error: any) {
+    if (error instanceof TokenAlreadyUsedError) {
+      return res.status(409).json({
+        code: PasswordResetCode.ALREADY_USED,
+      });
+    }
+
+    if (error instanceof TokenNotFoundError) {
+      return res.status(401).json({
+        code: PasswordResetCode.INVALID_TOKEN,
+      });
+    }
+
+    return res.status(500).json({
+      code: "INTERNAL_ERROR",
+    });
+  }
+
+  return res.status(200).json({ message: emailVerificationCodes.SUCCESS });
 };
