be6a7ed80f1b88db252f6d16df33c63713291429
RayyesAbed <abdullarayes00@gmail.com>
2025-12-16 19:13:10 +0100

    Threw BadRequestError in validateInputs when errors is not empty

--- src/features/auth/utils/validateInputs.ts
+++ src/features/auth/utils/validateInputs.ts
@@ -1,11 +1,12 @@
 import { Request } from "express";
 import { validationResult } from "express-validator";
+import { BadRequestError } from "../../../errors/BadRequestError";
 
 const validateInputs = (req: Request) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    errors.throw();
+    throw new BadRequestError("Invalid request data", errors.array());
   }
 };
 
