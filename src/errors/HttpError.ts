28b4a6c23b2416d92cd53479657fd09a15a17936
RayyesAbed <abdullarayes00@gmail.com>
2025-12-16 19:11:06 +0100

    Added optional details property to HttpError class

--- src/errors/HttpError.ts
+++ src/errors/HttpError.ts
@@ -1,10 +1,12 @@
 class HttpError extends Error {
   statusCode: number;
   isOperational = true;
+  details?: unknown;
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: number, details?: unknown) {
     super(message);
     this.statusCode = statusCode;
+    this.details = details;
   }
 }
 
