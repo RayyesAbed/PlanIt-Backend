87a271a6b373140ae962cf3a6cab8a7470b0207e
RayyesAbed <abdullarayes00@gmail.com>
2025-12-16 18:58:07 +0100

    Added handleControllerError function to handle client and server side errors:
    - If error is instance of HttpError, it is client side
    - Otherwise it is server side

---
+++ src/features/auth/utils/handleControllerError.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import HttpError from "../../../errors/HttpError";
+
+export default function handleControllerError(error: unknown, res: Response) {
+  if (error instanceof HttpError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  console.error("Unexpected error:", error);
+
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+}
