fcd270ce9cfecf26c136874f25c184b9b9ec6691
RayyesAbed <abdullarayes00@gmail.com>
2026-01-20 17:02:17 +0100

    Replaced 'message' property name with 'code' for clarity

--- src/features/auth/handlers/resetPasswordHandler.ts
+++ src/features/auth/handlers/resetPasswordHandler.ts
@@ -27,7 +27,7 @@ export const resetPasswordHandler = async (
       password: await argon2.hash(newPassword),
     });
 
-    return res.status(200).json({ message: emailVerificationCodes.SUCCESS });
+    return res.status(200).json({ code: emailVerificationCodes.SUCCESS });
   } catch (error) {
     return handleControllerError(error, res);
   }
