1af0d726bde65f8670e939b0e6d4c75d4a61f2f9
RayyesAbed <abdullarayes00@gmail.com>
2026-01-25 20:56:06 +0100

    Wrapped signJWT function inside try...catch block

--- src/features/auth/services/common/signJWT.ts
+++ src/features/auth/services/common/signJWT.ts
@@ -7,25 +7,29 @@ import { Types } from "mongoose";
 const signJWT = async (
   credentialsDTO: RegisterRequestDTO | ResetPasswordRequestDTO,
   jwtSignPurpose: string,
-  userId: Types.ObjectId
+  userId: Types.ObjectId,
 ) => {
-  const jti = randomUUID();
+  try {
+    const jti = randomUUID();
 
-  const { JWT_SECRET } = loadSecrets();
-  const verificationToken = jwt.sign(
-    {
-      userId: userId,
-      lang: credentialsDTO.preferredLanguage,
-      purpose: jwtSignPurpose,
-    },
-    JWT_SECRET,
-    {
-      expiresIn: "1h",
-      jwtid: jti,
-    }
-  );
+    const { JWT_SECRET } = loadSecrets();
+    const verificationToken = jwt.sign(
+      {
+        userId: userId,
+        lang: credentialsDTO.preferredLanguage,
+        purpose: jwtSignPurpose,
+      },
+      JWT_SECRET,
+      {
+        expiresIn: "1h",
+        jwtid: jti,
+      },
+    );
 
-  return { verificationToken, jti };
+    return { verificationToken, jti };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default signJWT;
