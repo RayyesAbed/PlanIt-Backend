c4f7a5f30caf60e985eb03730e4307b536bd6638
RayyesAbed <abdullarayes00@gmail.com>
2026-01-31 12:09:12 +0100

    Added cookieHeader and checked for its existence

--- src/features/auth/utils/getCookie.ts
+++ src/features/auth/utils/getCookie.ts
@@ -1,3 +1,6 @@
 import { Request } from "express";
 
-const getCookie = (req: Request, name: string) => {};
+const getCookie = (req: Request, name: string) => {
+  const cookieHeader = req.headers.cookie;
+  if (!cookieHeader) return null;
+};
