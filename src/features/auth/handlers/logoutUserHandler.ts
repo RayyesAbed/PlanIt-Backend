e5fb10cd346293f10fe7cf252fe8fb04eae38c6d
RayyesAbed <abdullarayes00@gmail.com>
2026-01-31 12:30:18 +0100

    Got and revoked Redis key and passed payload JTI to it

--- src/features/auth/handlers/logoutUserHandler.ts
+++ src/features/auth/handlers/logoutUserHandler.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import getCookie from "../utils/getCookie";
 import verifyJWT from "../services/common/verifyJWT";
+import getAndRevokeRedisKey from "../services/common/getAndRevokeRedisKey";
 
 export const logoutUserHandler = async (
   req: Request,
@@ -10,5 +11,7 @@ export const logoutUserHandler = async (
     const token = getCookie(req, "login")!;
 
     const payload = verifyJWT(token);
+
+    await getAndRevokeRedisKey(payload.jti as string);
   } catch (error) {}
 };
