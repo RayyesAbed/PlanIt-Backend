a4db07228c6595cbd2da819ca8409339efb192cf
RayyesAbed <abdullarayes00@gmail.com>
2026-01-31 12:20:48 +0100

    Used imported getCookie to return login token and verified JWT token and assigned it to payload

--- src/features/auth/handlers/logoutUserHandler.ts
+++ src/features/auth/handlers/logoutUserHandler.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
+import getCookie from "../utils/getCookie";
+import verifyJWT from "../services/common/verifyJWT";
 
 export const logoutUserHandler = async (
   req: Request,
   res: Response,
 ): Promise<any> => {
   try {
+    const token = getCookie(req, "login")!;
+
+    const payload = verifyJWT(token);
   } catch (error) {}
 };
