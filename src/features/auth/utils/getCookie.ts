fe3770278f1b8343e21d9b2057dce6718507c7c4
RayyesAbed <abdullarayes00@gmail.com>
2026-01-31 12:15:08 +0100

    Splitted cookieHeader and assigned it to cookies string array and looped through it

--- src/features/auth/utils/getCookie.ts
+++ src/features/auth/utils/getCookie.ts
@@ -3,4 +3,9 @@ import { Request } from "express";
 const getCookie = (req: Request, name: string) => {
   const cookieHeader = req.headers.cookie;
   if (!cookieHeader) return null;
+
+  const cookies = cookieHeader.split(";");
+
+  for (const cookie of cookies) {
+  }
 };
