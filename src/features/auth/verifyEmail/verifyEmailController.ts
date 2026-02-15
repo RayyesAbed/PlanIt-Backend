60c31cfef26217db92210cb6dab3e9d5049cedf8
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 17:15:07 +0100

    Make verifyEmailController async, use verifyEmailService and return success code

--- src/features/auth/verifyEmail/verifyEmailController.ts
+++ src/features/auth/verifyEmail/verifyEmailController.ts
@@ -1,15 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import handleControllerError from "../utils/handleControllerError";
 import verifyEmailService from "./verifyEmailService";
+import emailVerificationCodes from "../types/emailVerificationCodes";
 
-const verifyEmailController = (
+const verifyEmailController = async (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {
+): Promise<any> => {
   try {
     const token = req.query.token as string;
-    verifyEmailService(token);
+
+    await verifyEmailService(token);
+
+    return res.status(200).json({
+      code: emailVerificationCodes.SUCCESS,
+    });
   } catch (error) {
     return handleControllerError(error, res);
   }
