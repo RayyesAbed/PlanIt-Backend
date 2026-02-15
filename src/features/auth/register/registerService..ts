fe79f5b1e1c372bf51e44d0022a0399034347a68
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:07:11 +0100

    Check supported language from register credentials DTO

--- src/features/auth/register/registerService..ts
+++ src/features/auth/register/registerService..ts
@@ -1,8 +1,11 @@
 import { RegisterRequestDTO } from "../authDTOs";
+import checkSupportedLanguage from "../utils/checkSupportedLanguage";
 
 const registerService = (
   registerCredentialsDTO: RegisterRequestDTO,
   deviceIPv6: string,
-) => {};
+) => {
+  checkSupportedLanguage(registerCredentialsDTO.preferredLanguage);
+};
 
 export default registerService;
