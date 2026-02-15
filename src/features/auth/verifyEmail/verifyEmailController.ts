699149738cf85732dadb200ab5d97b624d8c5a43
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:48:25 +0100

    Use handleControllerError in catch block in verifyEmailController

--- src/features/auth/verifyEmail/verifyEmailController.ts
+++ src/features/auth/verifyEmail/verifyEmailController.ts
@@ -1,9 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import handleControllerError from "../utils/handleControllerError";
 
 const verifyEmailController = (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {};
+) => {
+  try {
+  } catch (error) {
+    return handleControllerError(error, res);
+  }
+};
 
 export default verifyEmailController;
