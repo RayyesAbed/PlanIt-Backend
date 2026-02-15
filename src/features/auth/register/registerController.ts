84a580a256b095edf29b6304d72af472f681096b
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:00:26 +0100

    Use handleControllerError in catch block in registerController

--- src/features/auth/register/registerController.ts
+++ src/features/auth/register/registerController.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import handleControllerError from "../utils/handleControllerError";
 
 const registerController = (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {};
+) => {
+  try {
+  } catch (error) {
+    console.error("Error registering user in controller:", error);
+    return handleControllerError(error, res);
+  }
+};
 
 export default registerController;
