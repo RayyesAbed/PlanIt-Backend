24a14f24d8c7e22acb1102b6076e0ffd171d634a
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:08:24 +0100

    Search for exisiting users if email is confirmed or to be confirmed

--- src/features/auth/register/registerService..ts
+++ src/features/auth/register/registerService..ts
@@ -1,11 +1,18 @@
 import { RegisterRequestDTO } from "../authDTOs";
 import checkSupportedLanguage from "../utils/checkSupportedLanguage";
+import User from "../../../schemas/User";
 
-const registerService = (
+const registerService = async (
   registerCredentialsDTO: RegisterRequestDTO,
   deviceIPv6: string,
 ) => {
   checkSupportedLanguage(registerCredentialsDTO.preferredLanguage);
+  const existingUser = await User.findOne({
+    $or: [
+      { confirmedEmail: registerCredentialsDTO.toBeConfirmedEmail },
+      { toBeConfirmedEmail: registerCredentialsDTO.toBeConfirmedEmail },
+    ],
+  });
 };
 
 export default registerService;
