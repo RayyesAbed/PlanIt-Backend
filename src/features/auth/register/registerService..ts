af99be87fa60c62e0727faeb0fa2a5f10aec5046
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:09:23 +0100

    Create new user if no user exists with given email

--- src/features/auth/register/registerService..ts
+++ src/features/auth/register/registerService..ts
@@ -1,6 +1,7 @@
 import { RegisterRequestDTO } from "../authDTOs";
 import checkSupportedLanguage from "../utils/checkSupportedLanguage";
 import User from "../../../schemas/User";
+import createNewUser from "../services/register/createNewUser";
 
 const registerService = async (
   registerCredentialsDTO: RegisterRequestDTO,
@@ -13,6 +14,9 @@ const registerService = async (
       { toBeConfirmedEmail: registerCredentialsDTO.toBeConfirmedEmail },
     ],
   });
+  if (!existingUser) {
+    const newUser = await createNewUser(registerCredentialsDTO, deviceIPv6);
+  }
 };
 
 export default registerService;
