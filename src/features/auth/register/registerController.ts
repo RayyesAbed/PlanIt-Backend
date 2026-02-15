7d0e3eef97416add3a883b4d11eb57c5c65eafbe
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:12:55 +0100

    Send register DTO and device IPv6 to register service

--- src/features/auth/register/registerController.ts
+++ src/features/auth/register/registerController.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { toRegisterDTO } from "../authDTOMappers";
 import handleControllerError from "../utils/handleControllerError";
+import registerService from "./registerService.";
 
-const registerController = (
+const registerController = async (
   req: Request,
   res: Response,
   next: NextFunction,
@@ -11,6 +12,7 @@ const registerController = (
     const registerCredentialsDTO = toRegisterDTO(req.body);
     const deviceIPv6 = req.ip;
     if (!deviceIPv6) throw new Error("Device IP not found");
+    await registerService(registerCredentialsDTO, deviceIPv6);
   } catch (error) {
     console.error("Error registering user in controller:", error);
     return handleControllerError(error, res);
