e66296d0d131f7696c6986203497bdca51500c08
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 16:53:00 +0100

    Find user by payload user id and store it in user

--- src/features/auth/verifyEmail/verifyEmailService.ts
+++ src/features/auth/verifyEmail/verifyEmailService.ts
@@ -1,9 +1,13 @@
 import verifyJWT from "../services/common/verifyJWT";
 import getAndRevokeRedisKey from "../services/common/getAndRevokeRedisKey";
+import User from "../../../schemas/User";
 
 const verifyEmailService = async (token: string) => {
   const payload = verifyJWT(token);
+
   await getAndRevokeRedisKey(payload.jti ?? "");
+
+  const user = await User.findById(payload.userId);
 };
 
 export default verifyEmailService;
