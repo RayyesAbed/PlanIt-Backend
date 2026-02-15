8ffab1ddc7c988d2c840f08495b73be9cb56f8aa
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 15:54:48 +0100

    Added registerController in /register directory

---
+++ src/features/auth/register/registerController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const registerController = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
+
+export default registerController;
