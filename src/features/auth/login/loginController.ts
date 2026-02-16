a02a68bc4d581dc2933d911222ba11d32e470eb8
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 19:30:21 +0100

    Use handleControllerError in catch block in loginController

--- src/features/auth/login/loginController.ts
+++ src/features/auth/login/loginController.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import handleControllerError from "../utils/handleControllerError";
 
 const loginController = async (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {};
+) => {
+  try {
+  } catch (error) {
+    console.error("Error logging user in controller:", error);
+    return handleControllerError(error, res);
+  }
+};
 
 export default loginController;
