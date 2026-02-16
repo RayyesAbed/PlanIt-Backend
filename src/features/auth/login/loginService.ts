38466db663b0e9ab5e79f8008ceee8c7d97bb5b6
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 19:42:22 +0100

    Check user existence and verify stored password with entered user password

--- src/features/auth/login/loginService.ts
+++ src/features/auth/login/loginService.ts
@@ -1,10 +1,18 @@
 import { LoginDTO } from "../authDTOs";
 import User from "../../../schemas/User";
+import * as argon2 from "argon2";
 
 const loginService = async (loginCredentialsDTO: LoginDTO) => {
   const existingUser = await User.findOne({
     confirmedEmail: loginCredentialsDTO.email,
   }).lean();
+
+  if (existingUser) {
+    const doPasswordsMatch = await argon2.verify(
+      existingUser.password,
+      loginCredentialsDTO.password,
+    );
+  }
 };
 
 export default loginService;
