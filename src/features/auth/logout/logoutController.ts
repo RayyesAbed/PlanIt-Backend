f67eb1955eaa354c332220805501ae941dc5c71a
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 21:06:11 +0100

    Use handleControllerError in catch block in logoutController

--- src/features/auth/logout/logoutController.ts
+++ src/features/auth/logout/logoutController.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from "express";
+import handleControllerError from "../utils/handleControllerError";
 
-const logoutController = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {};
+const logoutController = (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {
+    console.error("Error  logging out users in controller:", error);
+    handleControllerError(error, res);
+  }
+};
 
 export default logoutController;
