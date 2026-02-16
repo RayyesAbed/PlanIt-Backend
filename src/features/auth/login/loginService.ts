cd9f6d0e327c17560aa04883ffa62cc788b9120a
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 19:40:54 +0100

    Make loginService async and find existing user with login credentials email

--- src/features/auth/login/loginService.ts
+++ src/features/auth/login/loginService.ts
@@ -1,5 +1,10 @@
 import { LoginDTO } from "../authDTOs";
+import User from "../../../schemas/User";
 
-const loginService = (loginCredentialsDTO: LoginDTO) => {};
+const loginService = async (loginCredentialsDTO: LoginDTO) => {
+  const existingUser = await User.findOne({
+    confirmedEmail: loginCredentialsDTO.email,
+  }).lean();
+};
 
 export default loginService;
