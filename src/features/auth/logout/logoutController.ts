5abe007954e8f3117e9255f44070933f6a87cfdf
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 21:08:20 +0100

    Get login token with custom getCookie function and make logoutController async and return a promise that resolves to 'any' type

--- src/features/auth/logout/logoutController.ts
+++ src/features/auth/logout/logoutController.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import handleControllerError from "../utils/handleControllerError";
+import getCookie from "../utils/getCookie";
 
-const logoutController = (req: Request, res: Response, next: NextFunction) => {
+const logoutController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<any> => {
   try {
+    const token = getCookie(req, "login");
   } catch (error) {
     console.error("Error  logging out users in controller:", error);
     handleControllerError(error, res);
