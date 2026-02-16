ac7ad6d5020862880ef01e0170b828d58e87a994
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 21:11:12 +0100

    Verify login JWT token and assign it to payload

--- src/features/auth/logout/logoutService.ts
+++ src/features/auth/logout/logoutService.ts
@@ -1,3 +1,7 @@
-const logoutService = async (token: string) => {};
+import verifyJWT from "../services/common/verifyJWT";
+
+const logoutService = async (token: string) => {
+  const payload = verifyJWT(token);
+};
 
 export default logoutService;
