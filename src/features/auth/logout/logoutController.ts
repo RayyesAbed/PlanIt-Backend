614eb5c22ce6c0f0de2fb654e508c19ceae0abc3
RayyesAbed <abdullarayes00@gmail.com>
2026-02-16 21:04:13 +0100

    Add logoutController in /logout directory

---
+++ src/features/auth/logout/logoutController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const logoutController = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
+
+export default logoutController;
