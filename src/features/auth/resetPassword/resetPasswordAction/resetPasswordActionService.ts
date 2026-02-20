4f36d6e44a5c62ba160c3ee6eaa3c3d132d838f4
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 17:02:34 +0100

    Verify JWT token and assign it to decoded

--- src/features/auth/resetPassword/resetPasswordAction/resetPasswordActionService.ts
+++ src/features/auth/resetPassword/resetPasswordAction/resetPasswordActionService.ts
@@ -1,10 +1,13 @@
 import jwt from "jsonwebtoken";
+import verifyJWT from "../../services/common/verifyJWT";
 
 const resetPasswordActionService = async (
   token: string,
   newPassword: string,
 ) => {
   let decoded: jwt.JwtPayload;
+
+  decoded = verifyJWT(token);
 };
 
 export default resetPasswordActionService;
