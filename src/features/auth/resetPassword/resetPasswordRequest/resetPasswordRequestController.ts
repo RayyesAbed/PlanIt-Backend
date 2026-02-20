23a7e973372a1bbd00c43f7d8f078556efc29cf1
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 16:16:52 +0100

    Use handleControllerError in catch block in resetPasswordRequestController

--- src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestController.ts
+++ src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestController.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import handleControllerError from "../../utils/handleControllerError";
 
 const resetPasswordRequestController = async (
   req: Request,
   res: Response,
   next: NextFunction,
-): Promise<any> => {};
+): Promise<any> => {
+  try {
+  } catch (error) {
+    console.error("Error resetting user password in controller: ", error);
+    return handleControllerError(error, res);
+  }
+};
 
 export default resetPasswordRequestController;
