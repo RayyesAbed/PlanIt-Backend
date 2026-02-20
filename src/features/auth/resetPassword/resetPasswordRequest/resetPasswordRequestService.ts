26a9bf37cc8042226d97cfa2de98076f7bf359c9
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 16:20:03 +0100

    Use Mongoose findOne and pass confirmedEmail to it

--- src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
+++ src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
@@ -1,3 +1,7 @@
-const resetPasswordRequestService = async (confirmedEmail: string) => {};
+import User from "../../../../schemas/User";
+
+const resetPasswordRequestService = async (confirmedEmail: string) => {
+  const user = await User.findOne({ confirmedEmail: confirmedEmail });
+};
 
 export default resetPasswordRequestService;
