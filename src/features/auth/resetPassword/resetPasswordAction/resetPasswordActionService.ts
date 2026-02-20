ff4a2f2ba6aa7efe7020793d3cd505e2190d7ce8
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 17:01:59 +0100

    Add decoded of type JWT payload

--- src/features/auth/resetPassword/resetPasswordAction/resetPasswordActionService.ts
+++ src/features/auth/resetPassword/resetPasswordAction/resetPasswordActionService.ts
@@ -1,6 +1,10 @@
+import jwt from "jsonwebtoken";
+
 const resetPasswordActionService = async (
   token: string,
   newPassword: string,
-) => {};
+) => {
+  let decoded: jwt.JwtPayload;
+};
 
 export default resetPasswordActionService;
