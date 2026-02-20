1ed405f9307d6b79381a7b408b5b36ee8997cc42
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 16:21:12 +0100

    Map user Mongoose schema to reset password request DTO

--- src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
+++ src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
@@ -1,7 +1,12 @@
 import User from "../../../../schemas/User";
+import { toResetPasswordRequestDTO } from "../../authDTOMappers";
 
 const resetPasswordRequestService = async (confirmedEmail: string) => {
   const user = await User.findOne({ confirmedEmail: confirmedEmail });
+
+  if (user) {
+    const resetPasswordCredentials = toResetPasswordRequestDTO(user);
+  }
 };
 
 export default resetPasswordRequestService;
