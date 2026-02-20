48002a2fa2758fd772663caed6d6c634efaa4cd0
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 17:04:32 +0100

    Hash user new password and update password in User schema

--- src/features/auth/resetPassword/resetPasswordAction/resetPasswordActionService.ts
+++ src/features/auth/resetPassword/resetPasswordAction/resetPasswordActionService.ts
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import verifyJWT from "../../services/common/verifyJWT";
 import getAndRevokeRedisKey from "../../services/common/getAndRevokeRedisKey";
+import User from "../../../../schemas/User";
+import * as argon2 from "argon2";
 
 const resetPasswordActionService = async (
   token: string,
@@ -11,6 +13,10 @@ const resetPasswordActionService = async (
   decoded = verifyJWT(token);
 
   await getAndRevokeRedisKey(decoded.jti as string);
+
+  await User.findByIdAndUpdate(decoded.userId, {
+    password: await argon2.hash(newPassword),
+  });
 };
 
 export default resetPasswordActionService;
