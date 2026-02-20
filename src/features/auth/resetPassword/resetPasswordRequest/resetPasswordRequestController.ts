8d6b5ec0892c99233852a2e1618db270ecdc22b1
RayyesAbed <abdullarayes00@gmail.com>
2026-02-20 16:13:40 +0100

    Add resetPasswordRequestController in /resetPasswordRequest directory

---
+++ src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const resetPasswordRequestController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<any> => {};
+
+export default resetPasswordRequestController;
