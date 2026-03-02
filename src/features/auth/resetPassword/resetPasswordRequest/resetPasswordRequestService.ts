b888011d35d5923e02837e69834528a2d2f29210
RayyesAbed <abdullarayes00@gmail.com>
2026-03-02 13:45:03 +0100

    Prevent user enumeration in reset password flow
    Introduce fallback user data when email is not found to ensure
    consistent response behavior and avoid leaking account existence.

--- src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
+++ src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
@@ -3,24 +3,27 @@ import { toResetPasswordRequestDTO } from "../../authDTOMappers";
 import signJWT from "../../services/common/signJWT";
 import setRedisKey from "../../services/common/setRedisKey";
 import sendLinkWithEmail from "../../utils/sendLinkWithEmail";
+import fakeUserData from "./fakeUserData";
 
 const resetPasswordRequestService = async (confirmedEmail: string) => {
   const user = await User.findOne({ confirmedEmail: confirmedEmail });
 
-  const resetPasswordCredentials = toResetPasswordRequestDTO(user);
+  const existingUser = user ? user : fakeUserData;
+
+  const resetPasswordCredentials = toResetPasswordRequestDTO(existingUser);
 
   const { token, jti } = signJWT(
     resetPasswordCredentials,
     "reset_password",
-    user._id,
+    existingUser._id,
   );
 
   setRedisKey(jti, "false");
 
   await sendLinkWithEmail(
-    user.name,
-    user.confirmedEmail as string,
-    user.preferredLanguage,
+    existingUser.name,
+    existingUser.confirmedEmail as string,
+    existingUser.preferredLanguage,
     "passwordReset",
     token,
   );
