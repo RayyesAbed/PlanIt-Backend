26b0738a0d40bce5f591a1bd91e12313d485f849
RayyesAbed <abdullarayes00@gmail.com>
2026-03-03 18:50:30 +0100

    feat(auth): add configurable TTL to Redis JTI key

--- src/features/auth/services/common/setRedisKey.ts
+++ src/features/auth/services/common/setRedisKey.ts
@@ -1,8 +1,12 @@
 import { redis } from "../../../../configs/redis";
 
-const setRedisKey = async (jti: string, value: string) => {
+const setRedisKey = async (
+  jti: string,
+  value: string,
+  ttlInSeconds: number,
+) => {
   try {
-    await redis.set(`jti=${jti}`, value, "EX", 3600);
+    await redis.set(`jti=${jti}`, value, "EX", ttlInSeconds);
   } catch (error) {
     throw error;
   }
