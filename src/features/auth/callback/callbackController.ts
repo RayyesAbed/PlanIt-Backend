823ef43a675e26882b169acdaec3e9f5f4a4da6f
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 18:54:16 +0100

    feat(auth): redirect to frontend with JWT token

--- src/features/auth/callback/callbackController.ts
+++ src/features/auth/callback/callbackController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import handleControllerError from "../utils/handleControllerError";
 import callbackService from "./callbackService";
+import loadSecrets from "../../../configs/loadSecrets";
+
+const { FRONTEND_URL } = loadSecrets();
 
 const callbackController = async (
   req: Request,
@@ -17,7 +20,9 @@ const callbackController = async (
 
     if (!code) return res.status(400).send("No code provided");
 
-    callbackService(code, providerType, deviceIPv6);
+    const token = callbackService(code, providerType, deviceIPv6);
+
+    return res.redirect(`${FRONTEND_URL}/callback?token=${token}`);
   } catch (error) {
     console.error("Error in OAuth provider:", error);
     return handleControllerError(error, res);
