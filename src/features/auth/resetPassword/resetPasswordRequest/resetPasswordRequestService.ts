cf871a561a4d102ee1fe04271edb99f88db9f6f2
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 18:52:34 +0100

    refactor(auth): cast existing user and pass it to JWT signing function

--- src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
+++ src/features/auth/resetPassword/resetPasswordRequest/resetPasswordRequestService.ts
@@ -1,19 +1,17 @@
 import User from "../../../../schemas/User";
-import { toResetPasswordRequestDTO } from "../../authDTOMappers";
 import signJWT from "../../services/common/signJWT";
 import setRedisKey from "../../services/common/setRedisKey";
 import sendLinkWithEmail from "../../utils/sendLinkWithEmail";
 import fakeUserData from "./fakeUserData";
+import IUser from "../../../../interfaces/IUser";
 
 const resetPasswordRequestService = async (confirmedEmail: string) => {
   const user = await User.findOne({ confirmedEmail: confirmedEmail });
 
-  const existingUser = user ? user : fakeUserData;
-
-  const resetPasswordCredentials = toResetPasswordRequestDTO(existingUser);
+  const existingUser = (user ? user : fakeUserData) as IUser;
 
   const { token, jti } = signJWT(
-    resetPasswordCredentials,
+    existingUser,
     "reset_password",
     existingUser._id,
     900,
