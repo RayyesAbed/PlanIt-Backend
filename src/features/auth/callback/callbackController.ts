f845057d1b558f4097be224bb92a3030b61f0fc0
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 16:16:27 +0100

    feat(auth): add empty OAuth callback controller

---
+++ src/features/auth/callback/callbackController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const callbackController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<any> => {};
+
+export default callbackController;
