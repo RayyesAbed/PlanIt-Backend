c870251c243ced1b18344ab7576d87c17fb5c878
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 14:08:54 +0100

    feat(auth): add error handling with try...catch in controller

--- src/features/auth/provider/providerController.ts
+++ src/features/auth/provider/providerController.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import handleControllerError from "../utils/handleControllerError";
 
 const providerController = async (
   req: Request,
   res: Response,
   next: NextFunction,
-): Promise<any> => {};
+): Promise<any> => {
+  try {
+  } catch (error) {
+    console.error("Error in OAuth provider:", error);
+    return handleControllerError(error, res);
+  }
+};
 
 export default providerController;
