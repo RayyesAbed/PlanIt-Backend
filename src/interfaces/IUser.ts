34c9f3cd7394eaf016ce3b1d63561e629e1ba76e
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 18:32:27 +0100

    feat(auth): add Mongoose User interface

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,17 @@
+import { Document, Types } from "mongoose";
+
+interface IUser extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  confirmedEmail: string;
+  password?: string;
+  provider: "local" | "google" | "apple";
+  providerId?: string;
+  birthDate?: string;
+  preferredLanguage: string;
+  points: number;
+  currency: string;
+  subscription: any;
+}
+
+export default IUser;
