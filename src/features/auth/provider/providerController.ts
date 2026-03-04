87a5d161548a72c75db5d4c84f70266fbcb71808
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 13:57:58 +0100

    feat(auth): add empty OAuth provider controller

---
+++ src/features/auth/provider/providerController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const providerController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<any> => {};
+
+export default providerController;
