c4cf6f471ec3090298888466341d95c197287d31
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 17:36:26 +0100

    feat(auth): make User password optional

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -14,7 +14,6 @@ const UserSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
     },
     birthDate: {
       type: String,
@@ -40,6 +39,6 @@ const UserSchema = new mongoose.Schema(
   {
     timestamps: true,
     strict: true,
-  }
+  },
 );
 export default mongoose.model("User", UserSchema);
