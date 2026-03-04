fad29d33655dfa86a6d6ab535f9050716d3195c1
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 16:38:32 +0100

    feat(auth): extract Google OAuth data from loadSecrets function

--- src/features/auth/callback/callbackService.ts
+++ src/features/auth/callback/callbackService.ts
@@ -1,3 +1,11 @@
-const callbackService = (code: string) => {};
+import loadSecrets from "../../../configs/loadSecrets";
+
+const {
+  GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_REDIRECT_URI,
+  GOOGLE_OAUTH_CLIENT_SECRET,
+} = loadSecrets();
+
+const callbackService = (code: string, providerType: string) => {};
 
 export default callbackService;
