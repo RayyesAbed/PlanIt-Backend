3c55dcd984a13bc1d188d7c2dd0edd56d851e1af
RayyesAbed <abdullarayes00@gmail.com>
2026-03-04 14:59:26 +0100

    feat(auth): return custom OAuth links in provider service

---
+++ src/features/auth/provider/providerService.ts
@@ -0,0 +1,20 @@
+import enProviderType from "./enProviderType";
+import loadSecrets from "../../../configs/loadSecrets";
+
+const { GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_REDIRECT_URI } = loadSecrets();
+
+const providerService = (providerType: string) => {
+  if (providerType == enProviderType.GOOGLE) {
+    return (
+      `https://accounts.google.com/o/oauth2/v2/auth` +
+      `?client_id=${GOOGLE_OAUTH_CLIENT_ID}` +
+      `&redirect_uri=${GOOGLE_OAUTH_REDIRECT_URI}` +
+      "&response_type=code" +
+      "&scope=openid email profile"
+    );
+  } else if (providerType == enProviderType.APPLE) {
+    return; // TODO: I will update OAuthLink with Apple when I enroll in Apple Developer Program
+  }
+};
+
+export default providerService;
