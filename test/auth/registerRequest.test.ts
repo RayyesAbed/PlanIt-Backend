b353e0829115271976ddf5ac5b7c48c2560105df
RayyesAbed <abdullarayes00@gmail.com>
2026-03-12 15:17:44 +0100

    test(auth): add integration test to register new users

--- test/auth/registerRequest.test.ts
+++ test/auth/registerRequest.test.ts
@@ -1,3 +1,16 @@
 import { describe, it, expect } from "vitest";
+import request from "supertest";
+import app from "../../app";
 
-describe("POST /auth/register_request", () => {});
+describe("POST /auth/register_request", () => {
+  it("should register new users", async () => {
+    const response = await request(app).post("/auth/register_request").send({
+      name: "Fake",
+      toBeConfirmedEmail: "fakeuser@fake.com",
+      language: "en",
+      password: "test123Test456!",
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
