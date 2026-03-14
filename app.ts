4eeb78ed744d7296dda49d3a0329522f53f3549a
RayyesAbed <abdullarayes00@gmail.com>
2026-03-14 16:37:35 +0100

    fix(app): execute CORS middleware first to avoid CORS errors

--- app.ts
+++ app.ts
@@ -8,15 +8,15 @@ import authRoutes from "./src/features/auth/authRoutes";
 
 const app = express();
 
-app.use(express.json());
-
-app.use("/auth", authRoutes);
-
 app.use(
   cors({
     origin: ["http://localhost:5173"],
     credentials: true,
   }),
 );
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 export default app;
