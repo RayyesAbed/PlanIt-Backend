556e0aa1661b6d9e03a669cc3cf0dce3bac8b7e6
RayyesAbed <abdullarayes00@gmail.com>
2026-03-21 13:36:22 +0100

    test(auth): add integration test to reset existing user's password

--- test/auth/resetPassword.test.ts
+++ test/auth/resetPassword.test.ts
@@ -1,3 +1,32 @@
 import { describe, it, expect } from "vitest";
 import request from "supertest";
 import app from "../../app";
+import signJWT from "../../src/features/auth/services/common/signJWT";
+import createTestUser from "../utils/createTestUser";
+import setRedisKey from "../../src/features/auth/services/common/setRedisKey";
+import User from "../../src/schemas/User";
+import * as argon2 from "argon2";
+
+describe("PATCH /auth/reset-password", () => {
+  it("should update user's password with their new one", async () => {
+    const user = await createTestUser();
+    const newPassword = "new123Password!";
+    const resetPasswordPayload = signJWT(user, "password-reset", user._id, 600);
+
+    await setRedisKey(resetPasswordPayload.jti, "false", 600);
+    const response = await request(app)
+      .patch(`/auth/reset-password?token=${resetPasswordPayload.token}`)
+      .send({ newPassword });
+
+    expect(response.status).toBe(200);
+
+    const updatedUser = await User.findById(user._id);
+
+    const passwordMatches = await argon2.verify(
+      updatedUser?.password!,
+      newPassword,
+    );
+
+    expect(passwordMatches).toBe(true);
+  });
+});
