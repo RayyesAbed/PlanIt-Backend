952e1a30ea11bb076282baad47faba35eba4ee61
RayyesAbed <abdullarayes00@gmail.com>
2026-03-21 13:04:24 +0100

    test(auth): add integration test to verify existing user with valid JWT

--- test/auth/verifyEmail.test.ts
+++ test/auth/verifyEmail.test.ts
@@ -1,3 +1,20 @@
 import { describe, it, expect } from "vitest";
 import request from "supertest";
 import app from "../../app";
+import createTestUser from "../utils/createTestUser";
+import signJWT from "../../src/features/auth/services/common/signJWT";
+import setRedisKey from "../../src/features/auth/services/common/setRedisKey";
+
+describe("POST /auth/verify-email", () => {
+  it("should verify a user email with a valid token", async () => {
+    const user = await createTestUser();
+    const payload = signJWT(user, "email-verification-test", user._id, 600);
+
+    await setRedisKey(payload.jti, "false", 600);
+    const response = await request(app).post(
+      `/auth/verify-email?token=${payload.token}`,
+    );
+
+    expect(response.status).toBe(200);
+  });
+});
