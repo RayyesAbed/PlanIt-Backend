aebe187d6551405db0fd0659a714cf7d958fa71d
RayyesAbed <abdullarayes00@gmail.com>
2026-04-05 10:08:02 +0200

    feat(app): create task-adding mutation function

--- src/graphql/resolvers/resolvers.ts
+++ src/graphql/resolvers/resolvers.ts
@@ -1,3 +1,4 @@
+import toTaskDTO from "../../features/tasks/taskDTOMappers";
 import Task from "../../schemas/Task";
 
 const resolvers = {
@@ -8,6 +9,19 @@ const resolvers = {
     getUserTasks: async (_: any, __: any, contextValue: any) => {
       const userTask = await Task.findOne({ userId: contextValue.user._id });
       return userTask?.list;
+    },
+  },
+  Mutation: {
+    addTask: async (_: any, args: any, contextValue: any) => {
+      const userTask = await Task.findOne({ userId: contextValue.user._id });
+
+      const taskDTO = toTaskDTO(args);
+
+      userTask?.list.push(taskDTO);
+
+      await userTask?.save();
+
+      return args.input;
     },
   },
 };
