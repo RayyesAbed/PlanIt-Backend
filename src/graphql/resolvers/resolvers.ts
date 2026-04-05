00a54ed4bbf83710ad85c952fe79a0b18b086cb4
RayyesAbed <abdullarayes00@gmail.com>
2026-04-05 09:21:22 +0200

    feat(app): find corresponding task for a user and return it

--- src/graphql/resolvers/resolvers.ts
+++ src/graphql/resolvers/resolvers.ts
@@ -1,7 +1,13 @@
+import Task from "../../schemas/Task";
+
 const resolvers = {
   Query: {
     getUserData: (_: any, __: any, contextValue: any) => {
       return contextValue.user;
+    },
+    getUserTasks: async (_: any, __: any, contextValue: any) => {
+      const userTask = await Task.findOne({ userId: contextValue.user._id });
+      return userTask?.list;
     },
   },
 };
