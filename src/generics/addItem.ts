7ccd65ff10471f5a0b852c457231c6e19fa507be
RayyesAbed <abdullarayes00@gmail.com>
2026-05-17 13:04:26 +0200

    feat(app): add generic and parameters to addItem to use it in adding Tasks, Projects, ...etc

--- src/generics/addItem.ts
+++ src/generics/addItem.ts
@@ -1,3 +1,10 @@
-const addItem = () => {};
+import { Model } from "mongoose";
+import IDocument from "../types/IDocument";
+
+const addItem = async <Type extends IDocument<Type>>(
+  model: Model<Type>,
+  contextValue: any,
+  itemDTO: any,
+) => {};
 
 export default addItem;
