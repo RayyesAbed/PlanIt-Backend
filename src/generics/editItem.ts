08958466870d5c6715e49a393370e309db10e733
RayyesAbed <abdullarayes00@gmail.com>
2026-06-22 14:28:50 +0200

    feat(app): add editItem generic to edit various data types

--- src/generics/editItem.ts
+++ src/generics/editItem.ts
@@ -1,2 +1,24 @@
 import { Model } from "mongoose";
 import IDocument from "../types/IDocument";
+
+const editItem = async <Type extends IDocument<Type>>(
+  model: Model<Type>,
+  contextValue: any,
+  itemDTO: any,
+) => {
+  await model.updateOne(
+    {
+      userId: contextValue.user._id,
+      "list._id": itemDTO._id,
+    },
+    {
+      $set: {
+        "list.$": itemDTO,
+      },
+    },
+  );
+
+  return itemDTO;
+};
+
+export default editItem;
