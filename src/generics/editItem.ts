549ebf588215f52dc3eb6b2dc0bbdb1372d0cda4
RayyesAbed <abdullarayes00@gmail.com>
2026-06-28 13:24:29 +0200

    refactor(app): use context value type in editItem generic for type safety

--- src/generics/editItem.ts
+++ src/generics/editItem.ts
@@ -1,14 +1,15 @@
 import { Model } from "mongoose";
 import IDocument from "../types/IDocument";
+import { ContextValue } from "../types/graphqlTypes";
 
 const editItem = async <Type extends IDocument<Type>>(
   model: Model<Type>,
-  contextValue: any,
+  contextValue: ContextValue | null,
   itemDTO: any,
 ) => {
   await model.updateOne(
     {
-      userId: contextValue.user._id,
+      userId: contextValue?.user._id,
       "list._id": itemDTO._id,
     },
     {
